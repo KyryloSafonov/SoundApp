9ef34aeb2d09c596c4e2d34de3e43eda1a3ecafa
pc <kyrylo.safonov.kb.2019@lpnu.ua>
2021-04-23 17:59:58 +0300

    need to add play audio

--- screens/AudioScreen.js
+++ screens/AudioScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import styleAudioScreen from '../styles/styleLoginScreen';
+import SoundPlayer from 'react-native-sound-player';
 
 const AudioScreen = ({navigation}) => {
+  const Play = () => {
+    try {
+      // SoundPlayer.playSoundFile('tone', 'mp3');
+      SoundPlayer.playUrl(
+        'https://music.youtube.com/watch?v=ibc3bFHNbdg&list=LM',
+      );
+    } catch (e) {
+      console.log('cannot play the sound file', e);
+    }
+  };
   return (
     <View style={styleAudioScreen.screen}>
-      <Text>Audio Screen</Text>
+      <Button title="Play" onPress={Play} />
+      <Button title="Pause" />
     </View>
   );
 };
--- styles/styleLoginScreen.js
+++ styles/styleLoginScreen.js
@@ -3,7 +3,8 @@ import {StyleSheet} from 'react-native';
 const styleLoginScreen = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
 });
